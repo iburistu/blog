3599cd8c76270dc38c9fe71705c2fad35484c8e2
iburistu <zack@linkletter.dev>
2022-08-09 19:28:13 -0400

    fix: GA tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -146,5 +146,11 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [process.env.GA_TRACKING_ID]
+      }
+    }
   ],
 }
